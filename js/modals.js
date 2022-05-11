cf18bda0221167ed9d99dc54684f07f93f6baf9b
Myron <bruscel96@gmail.com>
2022-05-11 00:04:28 -0400

    first commit

---
+++ js/modals.js
@@ -0,0 +1,76 @@
+//Modal 1
+const toggleModal = () => {
+    document.querySelector('.modal')
+    .classList.toggle('modal--hidden');
+
+};
+document.querySelector('#show-modal')
+.addEventListener('click', toggleModal);
+
+document.querySelector('.modal_close_bar')
+.addEventListener('click',toggleModal);
+
+//Modal2
+const toggleModal2 = () => {
+    document.querySelector('.modal2')
+    .classList.toggle('modal--hidden2');
+
+};
+    document.querySelector('#show-modal2')
+    .addEventListener('click', toggleModal2);
+
+    document.querySelector('.modal_close_bar2')
+    .addEventListener('click',toggleModal2);
+
+//Modal3
+const toggleModal3 = () => {
+    document.querySelector('.modal3')
+    .classList.toggle('modal--hidden3');
+
+};
+    document.querySelector('#show-modal3')
+    .addEventListener('click', toggleModal3);
+
+    document.querySelector('.modal_close_bar3')
+    .addEventListener('click',toggleModal3);
+
+
+//Modal4
+const toggleModal4 = () => {
+    document.querySelector('.modal4')
+    .classList.toggle('modal--hidden4');
+
+};
+    document.querySelector('#show-modal4')
+    .addEventListener('click', toggleModal4);
+
+    document.querySelector('.modal_close_bar4')
+    .addEventListener('click',toggleModal4);
+
+
+//Modal5
+const toggleModal5 = () => {
+    document.querySelector('.modal5')
+    .classList.toggle('modal--hidden5');
+
+};
+    document.querySelector('#show-modal5')
+    .addEventListener('click', toggleModal5);
+
+    document.querySelector('.modal_close_bar5')
+    .addEventListener('click',toggleModal5);
+
+    
+//Modal6
+const toggleModal6 = () => {
+    document.querySelector('.modal6')
+    .classList.toggle('modal--hidden6');
+
+};
+    document.querySelector('#show-modal6')
+    .addEventListener('click', toggleModal6);
+
+    document.querySelector('.modal_close_bar6')
+    .addEventListener('click',toggleModal6);
+
+   
